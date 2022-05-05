946c6a67137123d9d144be21e5c724b57c263241
Dilan Suárez <74948783+DilanSA@users.noreply.github.com>
2022-05-05 13:53:19 -0500

    Update DataExploratoryAnalysis.R

--- DataExploratoryAnalysis.R
+++ DataExploratoryAnalysis.R
@@ -6,7 +6,7 @@
 
 ##------------------------------------------------------------------------------------
 
-#### INSTALACI??N Y ACTIVACI??NN DE PAQUETES
+#### INSTALACIÓN Y ACTIVACIÓN DE PAQUETES
 
 #
 install.packages("dplyr")
@@ -20,7 +20,7 @@ library(ggplot2)
 
 #### IMPORTANDO LA BASE DE DATOS DE R
 
-#Para este ejercicio utilizaremos la base de datos CO2 "Carbon Dioxide Uptake in Grass Plants", correspondiente a un experimento sobre la tolerancia al fr??o de la especie de pasto Echinochloa crus-galli.
+#Para este ejercicio utilizaremos la base de datos CO2 "Carbon Dioxide Uptake in Grass Plants", correspondiente a un experimento sobre la tolerancia al frío de la especie de pasto Echinochloa crus-galli.
   
 data("CO2") #Importaci??n de dataset
 CO2         #Visualizaci??n parcial en consola
@@ -28,13 +28,13 @@ View(CO2)   #Visualizaci??n completa en una nueva ventana
 
 #### UN VISTAZO A LOS DATOS
 
-#Para la obtenci??n de algunos detalles sobre c??mo est???? estructurada la base de datos, utilizaremos la funci??n `glimpse()` contenida dentro del paquete dplyr.
+#Para la obtención de algunos detalles sobre cómo está estructurada la base de datos, utilizaremos la función glimpse() contenida dentro del paquete dplyr.
   
-#Esta funci??n nos brinda informaci??n sobre:
+#Esta función nos brinda información sobre:
   
-# -El n??mero de filas (observaciones) y columnas (variables) que presenta la base de datos.
+# -El número de filas (observaciones) y columnas (variables) que presenta la base de datos.
 
-# -La naturaleza de los elementos de cada variable (columna): \<*ord*\> , \<*fct*\>, \<*dbl*\>.
+# -La naturaleza de los elementos de cada variable (columna): <ord> , <fct>, <dbl>.
 
 # -Contenido de cada variable (columna) en forma de lista.
 
@@ -43,9 +43,9 @@ View(CO2)   #Visualizaci??n completa en una nueva ventana
 glimpse(CO2)
 is.na(CO2)
 
-#### AN??LISIS DE VARIABLES CATEG??RICAS
+#### ANÁLISIS DE VARIABLES CATEGÓRICAS
 
-#Para las variables categ??ricas podemos calcular tablas de frecuencia, es decir, ver el n??mero de ocurrencias de cada categor??a de la variable. Esto lo hacemos con la funci??n table().
+#Para las variables categóricas podemos calcular tablas de frecuencia, es decir, ver el número de ocurrencias de cada categoría de la variable. Esto lo hacemos con la función table().
 
 table(CO2$Plant)
 table(CO2$Type)
@@ -53,7 +53,7 @@ table(CO2$Treatment)
 
 #### TABLAS DE CONTINGENCIA
 
-#Para poder evaluar la frecuencia de dos variables categ??ricas podemos utilizar tablas de contingencia. Esto lo hacemos con la funci??n prop.table()
+#Para poder evaluar la frecuencia de dos variables categ??ricas podemos utilizar tablas de contingencia. Esto lo hacemos con la función prop.table()
 #Tablas de contingencia para cada combinaci??n de variables categ??ricas. Valores expresados en porcentajes.
 
 Plant_vs_Type <- prop.table(table(CO2$Plant, CO2$Type),2)*100
@@ -63,11 +63,11 @@ Type_vs_Treatment
 Plant_vs_Treatment <- prop.table(table(CO2$Plant, CO2$Treatment),2)*100
 Plant_vs_Treatment
 
-### AN??LISIS DE VARIABLES NUM??RICAS
+### ANÁLISIS DE VARIABLES NUMÉRICAS
 
-#Para calcular los estad??sticos descriptivos de las variables num??ricas utilizaremos la funci??n summary(). Esta funci??n de puede aplicar a toda la base de datos o solo a variables espec??ficas dentro de mi base de datos. Los valores que nos brinda son:
+#Para calcular los estadísticos descriptivos de las variables numéricas utilizaremos la función summary(). Esta función de puede aplicar a toda la base de datos o solo a variables específicas dentro de mi base de datos. Los valores que nos brinda son:
   
-# -Valor m??nimo
+# -Valor mínimo
 
 # -Valor del 1er cuartil
 
@@ -77,44 +77,44 @@ Plant_vs_Treatment
 
 # -Valor del 3er cuartil
 
-# -Valor m??ximo
+# -Valor máximo
 
 summary(CO2$conc)
 summary(CO2$uptake)
 
-#### VISUALIZACI??N DE DATOS NUM??RICOS
+#### VISUALIZACIÓN DE DATOS NUMÉRICOS
 
-#Una forma ??til de explorar la distribuci??n de los datos en variables num??ricas es con histogramas. Estos podemos hacerlos con el comando hist(), donde ponemos como argumento el nombre de la tabla seguido de $ y el nombre de la variable que deseamos graficar.
+#Una forma útil de explorar la distribución de los datos en variables numéricas es con histogramas. Estos podemos hacerlos con el comando hist(), donde ponemos como argumento el nombre de la tabla seguido de $ y el nombre de la variable que deseamos graficar.
 
 hist(CO2$conc, col = "pink", border = "red", xlab ="Concentration (ml/L)", main = "Histogram")
 hist(CO2$uptake, col = "gray", border ="black", xlab ="Uptake (umol/m^2 sec)", main = "Histogram")
 
 #### CORRELACIONES
 
-#Una correlaci??n mide la relaci??n lineal entre dos variables. De esta manera podemos establecer si una variable est???? influenciada por otra de forma positiva o negativa.
+#Una correlaci??n mide la relaci??n lineal entre dos variables. De esta manera podemos establecer si una variable está influenciada por otra de forma positiva o negativa.
 
-#El resultado de este an??lisis es un coeficiente que va de -1 a 1
+#El resultado de este análisis es un coeficiente que va de -1 a 1
 
-# -R = 1, la relaci??n es positiva perfecta
+# -R = 1, la relación es positiva perfecta
 
-# -0 \< R \< 1 la relaci??n es positiva
+# -0 \< R \< 1 la relación es positiva
 
-# -R = 0 no hay relaci??n lineal
+# -R = 0 no hay relación lineal
 
-# - -1 \< R \< 0 la relaci??n es negativa
+# - -1 \< R \< 0 la relación es negativa
 
-# -R = -1 la relaci??n es negativa perfecta
+# -R = -1 la relación es negativa perfecta
 
-#C??lculo del coeficiente de correlaci??n para las dos variables num??ricas
+#Cálculo del coeficiente de correlación para las dos variables numéricas
 
 cor(CO2$conc, CO2$uptake)
 
-#Creaci??n de una tabla que solo contenga las dos variables num??ricas de la base de datos CO2
+#Creación de una tabla que solo contenga las dos variables num??ricas de la base de datos CO2
 
 cor <- data.frame(Concentration = CO2$conc, Uptake = CO2$uptake)
 cor
 
-#Creaci??n de gr??fica con ggplot2
+#Creación de gráfica con ggplot2
 
 ggplot(cor, mapping = aes(x = Concentration, y = Uptake)) +
   geom_point(color="blue", size = 1.5) +
