e10bc6458057000dd41316b42a114e17d62e1e49
DilanSA <dsuarezaguero@gmail.com>
2022-05-05 12:26:42 -0500

    Update DataExploratoryAnalysis

--- DataExploratoryAnalysis.R
+++ DataExploratoryAnalysis.R
@@ -1,12 +1,12 @@
-############################################
-## Title: "ANÁLISIS EXPLORATORO DE DATOS" ##
-## Author: "Dilan Suárez Agüero"          ##
-## Date: "05/05/2022"                     ##
-############################################
+#############################################
+## Title:  "AN??LISIS EXPLORATORO DE DATOS" ##
+## Author: "DILAN SU??REZ AG??ERO"           ##
+## Date:   "05/05/2022"                    ##
+#############################################
 
 ##------------------------------------------------------------------------------------
 
-#### INSTALACIÓN Y ACTIVACIÓN DE PAQUETES
+#### INSTALACI??N Y ACTIVACI??NN DE PAQUETES
 
 install.packages("dplyr")
 install.packages("corrplot")
@@ -18,32 +18,32 @@ library(ggplot2)
 
 #### IMPORTANDO LA BASE DE DATOS DE R
 
-#Para este ejercicio utilizaremos la base de datos `CO2` "Carbon Dioxide Uptake in Grass Plants", correspondiente a un experimento sobre la tolerancia al frío de la especie de pasto *Echinochloa crus-galll.*
+#Para este ejercicio utilizaremos la base de datos CO2 "Carbon Dioxide Uptake in Grass Plants", correspondiente a un experimento sobre la tolerancia al fr??o de la especie de pasto Echinochloa crus-galli.
   
-data("CO2") #Importación de dataset
-CO2 #Visualización parcial en consola
-View(CO2) #Visualización completa en una nueva ventana
+data("CO2") #Importaci??n de dataset
+CO2         #Visualizaci??n parcial en consola
+View(CO2)   #Visualizaci??n completa en una nueva ventana
 
 #### UN VISTAZO A LOS DATOS
 
-#Para la obtención de algunos detalles sobre cómo está estructurada la base de datos, utilizaremos la función `glimpse()` contenida dentro del paquete **dplyr.**
+#Para la obtenci??n de algunos detalles sobre c??mo est???? estructurada la base de datos, utilizaremos la funci??n `glimpse()` contenida dentro del paquete dplyr.
   
-#Esta función nos brinda información sobre:
+#Esta funci??n nos brinda informaci??n sobre:
   
-# -El número de filas (observaciones) y columnas (variables) que presenta la base de datos.
+# -El n??mero de filas (observaciones) y columnas (variables) que presenta la base de datos.
 
 # -La naturaleza de los elementos de cada variable (columna): \<*ord*\> , \<*fct*\>, \<*dbl*\>.
 
 # -Contenido de cada variable (columna) en forma de lista.
 
-#Es importante también identificar la presencia de valores perdidos o "NA values" para que no generen conflictos al momento de realizar análisis estadísticos posteriores, para ello usaremos la función `is.na().`
+#Es importante tambi??n identificar la presencia de valores perdidos o "NA values" para que no generen conflictos al momento de realizar an??lisis estad??sticos posteriores, para ello usaremos la funci??n is.na().
 
 glimpse(CO2)
 is.na(CO2)
 
-#### ANÁLISIS DE VARIABLES CATEGÓRICAS
+#### AN??LISIS DE VARIABLES CATEG??RICAS
 
-#Para las variables categóricas podemos calcular tablas de frecuencia, es decir, ver el número de ocurrencias de cada categoría de la variable. Esto lo hacemos con la función `table()`.
+#Para las variables categ??ricas podemos calcular tablas de frecuencia, es decir, ver el n??mero de ocurrencias de cada categor??a de la variable. Esto lo hacemos con la funci??n table().
 
 table(CO2$Plant)
 table(CO2$Type)
@@ -51,8 +51,8 @@ table(CO2$Treatment)
 
 #### TABLAS DE CONTINGENCIA
 
-#Para poder evaluar la frecuencia de dos variables categóricas podemos utilizar tablas de contingencia. Esto lo hacemos con la función `prop.table()`
-#Tablas de contingencia para cada combinación de variables categóricas. Valores expresados en porcentajes.
+#Para poder evaluar la frecuencia de dos variables categ??ricas podemos utilizar tablas de contingencia. Esto lo hacemos con la funci??n prop.table()
+#Tablas de contingencia para cada combinaci??n de variables categ??ricas. Valores expresados en porcentajes.
 
 Plant_vs_Type <- prop.table(table(CO2$Plant, CO2$Type),2)*100
 Plant_vs_Type
@@ -61,11 +61,11 @@ Type_vs_Treatment
 Plant_vs_Treatment <- prop.table(table(CO2$Plant, CO2$Treatment),2)*100
 Plant_vs_Treatment
 
-### ANÁLISIS DE VARIABLES NUMÉRICAS
+### AN??LISIS DE VARIABLES NUM??RICAS
 
-#Para calcular los estadísticos descriptivos de las variables numéricas utilizaremos la función `summary()`. Esta función de puede aplicar a toda la base de datos o solo a variables específicas dentro de mi base de datos. Los valores que nos brinda son:
+#Para calcular los estad??sticos descriptivos de las variables num??ricas utilizaremos la funci??n summary(). Esta funci??n de puede aplicar a toda la base de datos o solo a variables espec??ficas dentro de mi base de datos. Los valores que nos brinda son:
   
-# -Valor mínimo
+# -Valor m??nimo
 
 # -Valor del 1er cuartil
 
@@ -75,44 +75,44 @@ Plant_vs_Treatment
 
 # -Valor del 3er cuartil
 
-# -Valor máximo
+# -Valor m??ximo
 
 summary(CO2$conc)
 summary(CO2$uptake)
 
-#### VISUALIZACIÓN DE DATOS NUMÉRICOS
+#### VISUALIZACI??N DE DATOS NUM??RICOS
 
-#Una forma útil de explorar la distribución de los datos en variables numéricas es con histogramas. Estos podemos hacerlos con el comando `hist()`, donde ponemos como argumento el nombre de la tabla seguido de `$` y el nombre de la variable que deseamos graficar.
+#Una forma ??til de explorar la distribuci??n de los datos en variables num??ricas es con histogramas. Estos podemos hacerlos con el comando hist(), donde ponemos como argumento el nombre de la tabla seguido de $ y el nombre de la variable que deseamos graficar.
 
 hist(CO2$conc, col = "pink", border = "red", xlab ="Concentration (ml/L)", main = "Histogram")
 hist(CO2$uptake, col = "gray", border ="black", xlab ="Uptake (umol/m^2 sec)", main = "Histogram")
 
 #### CORRELACIONES
 
-#Una correlación mide la relación lineal entre dos variables. De esta manera podemos establecer si una variable está influenciada por otra de forma positiva o negativa.
+#Una correlaci??n mide la relaci??n lineal entre dos variables. De esta manera podemos establecer si una variable est???? influenciada por otra de forma positiva o negativa.
 
-#El resultado de este análisis es un coeficiente que va de -1 a 1
+#El resultado de este an??lisis es un coeficiente que va de -1 a 1
 
-# -R = 1, la relación es positiva perfecta
+# -R = 1, la relaci??n es positiva perfecta
 
-# -0 \< R \< 1 la relación es positiva
+# -0 \< R \< 1 la relaci??n es positiva
 
-# -R = 0 no hay relación lineal
+# -R = 0 no hay relaci??n lineal
 
-# - -1 \< R \< 0 la relación es negativa
+# - -1 \< R \< 0 la relaci??n es negativa
 
-# -R = -1 la relación es negativa perfecta
+# -R = -1 la relaci??n es negativa perfecta
 
-#Cálculo del coeficiente de correlación para las dos variables numéricas
+#C??lculo del coeficiente de correlaci??n para las dos variables num??ricas
 
 cor(CO2$conc, CO2$uptake)
 
-#Creación de una tabla que solo contenga las dos variables numéricas de la base de datos CO2
+#Creaci??n de una tabla que solo contenga las dos variables num??ricas de la base de datos CO2
 
 cor <- data.frame(Concentration = CO2$conc, Uptake = CO2$uptake)
 cor
 
-#Creación de gráfica con ggplot2
+#Creaci??n de gr??fica con ggplot2
 
 ggplot(cor, mapping = aes(x = Concentration, y = Uptake)) +
   geom_point(color="blue", size = 1.5) +
@@ -124,4 +124,3 @@ ggplot(cor, mapping = aes(x = Concentration, y = Uptake)) +
         panel.border = element_blank(), 
         panel.background = element_blank()) +
   theme_bw() + theme(plot.title = element_text(hjust = 0.5)) 
-```
